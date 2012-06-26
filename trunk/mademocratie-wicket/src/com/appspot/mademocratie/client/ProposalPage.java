0992541b5a6d7ea6c142966a02c7a27b4452566c
BVandeputte@gmail.com <BVandeputte@gmail.com@fe4b27e5-e7d9-34ed-cd55-3f530525dfba>
2012-06-26 08:28:49 +0000

    add proposal title & infos / minor css change

--- trunk/mademocratie-wicket/src/com/appspot/mademocratie/client/ProposalPage.java
+++ trunk/mademocratie-wicket/src/com/appspot/mademocratie/client/ProposalPage.java
@@ -1,10 +1,17 @@
 package com.appspot.mademocratie.client;
 
+import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.string.StringValue;
 
 import com.appspot.mademocratie.client.common.FooterPanel;
 import com.appspot.mademocratie.client.common.HeaderPanel;
+import com.appspot.mademocratie.model.Proposal;
+import com.appspot.mademocratie.server.service.IRepository;
+import com.google.inject.Inject;
 
 public class ProposalPage extends WebPage {
     /**
@@ -16,7 +23,9 @@ public class ProposalPage extends WebPage {
 	//~design
     ProposalPage page;
     
-
+    @Inject
+    private IRepository<Proposal> proposalRepo;
+    
     public ProposalPage(PageParameters params) {
         super(params);
 //        initServices();
@@ -27,9 +36,38 @@ public class ProposalPage extends WebPage {
     private void initComponents() {
     	createCommons();
         createBreadCrumbs();
+        createProposalDescription();
     }  
     
-    private void createCommons() {
+    private void createProposalDescription() {
+    	PageParameters params = this.getPageParameters();
+    	StringValue propIdStr = (params != null ? params.get("proposalId") : null);
+    	Long propId = (propIdStr != null ? propIdStr.toLong() : null);
+    	Proposal p = (propId != null ? proposalRepo.get(propId) : null);
+    	if (p == null) {
+    		getSession().error("Unable to retrieve the proposal");
+        	throw new RestartResponseException(HomePage.class, null);    		
+    	}
+    	String author = p.getAuthor() != null ? p.getAuthor().getNickname() : "An anonymous person ";
+    	String pDescString = p.getContent();
+
+        WebMarkupContainer pDescContainer = new WebMarkupContainer("pdesc-container");
+        add(pDescContainer);
+    	
+    	Label proposalDate = new Label("proposalDate", p.getDate().toString());
+    	Label proposalAuthor = new Label("proposalAuthor", author);
+    	Label proposalTitle = new Label("proposalTitle", p.getTitle());
+    	Label proposalDesc = new Label("proposalDescription", pDescString);
+    	
+    	add(proposalDate);
+    	add(proposalAuthor);
+    	add(proposalTitle);
+    	pDescContainer.add(proposalDesc);
+    	
+    	pDescContainer.setVisible(pDescString != null);
+	}
+
+	private void createCommons() {
     	HeaderPanel headerPanel = new HeaderPanel("headerPanel", page);
     	FooterPanel footerPanel = new FooterPanel("footerPanel", page);
         add(headerPanel);
