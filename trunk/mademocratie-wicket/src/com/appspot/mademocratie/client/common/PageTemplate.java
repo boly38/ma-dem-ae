9d9803dd80cb3054656018a3f65ee8cc3121e16e
BVandeputte@gmail.com <BVandeputte@gmail.com@fe4b27e5-e7d9-34ed-cd55-3f530525dfba>
2012-07-06 15:05:08 +0000

    add template

---
+++ trunk/mademocratie-wicket/src/com/appspot/mademocratie/client/common/PageTemplate.java
@@ -0,0 +1,45 @@
+package com.appspot.mademocratie.client.common;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.appspot.mademocratie.server.MaDemocratieApp;
+
+public class PageTemplate extends WebPage {
+    /**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = -5140576510124285604L;
+	/*
+     * page parameters
+     */
+    protected PageParameters params;    
+    /**
+     * application
+     */
+    @SuppressWarnings("unused")
+	private transient MaDemocratieApp app;
+    /**
+     * PageTemplate constructor
+     * @param params - page parameters map
+     */
+    public PageTemplate(final PageParameters params) {
+        this.params = params;
+        app = (MaDemocratieApp)getApplication();
+        createCommons();
+        setPagetitle();
+    }
+    
+    private void createCommons() {
+    	HeaderPanel headerPanel = new HeaderPanel("headerPanel", this);
+    	FooterPanel footerPanel = new FooterPanel("footerPanel", this);
+        add(headerPanel);
+        add(footerPanel);
+    }
+
+    protected void setPagetitle() {
+        /* set head page title to display in browser title bar */
+        add(new Label("head_page_title", "MaDemAe"));
+    }
+}
