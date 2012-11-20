acff1fb9e8c81964feef6cb100703cc6b014c285
BVandeputte@gmail.com <BVandeputte@gmail.com@fe4b27e5-e7d9-34ed-cd55-3f530525dfba>
2012-11-20 13:10:29 +0000

    proposal<->author : oneToMany attemp (failed)

--- trunk/mademocratie-gae/src/main/java/net/mademocratie/gae/server/service/impl/ManageProposalImpl.java
+++ trunk/mademocratie-gae/src/main/java/net/mademocratie/gae/server/service/impl/ManageProposalImpl.java
@@ -1,31 +1,38 @@
 package net.mademocratie.gae.server.service.impl;
 
+import com.google.appengine.api.datastore.Key;
 import com.google.inject.Inject;
 import net.mademocratie.gae.model.Citizen;
 import net.mademocratie.gae.model.Proposal;
-import net.mademocratie.gae.server.CitizenSession;
 import net.mademocratie.gae.server.service.IManageProposal;
 import net.mademocratie.gae.server.service.IProposal;
 import net.mademocratie.gae.server.service.IRepository;
 
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @DevInProgress
  */
 public class ManageProposalImpl implements IManageProposal {
+    private final static Logger LOGGER = Logger.getLogger(ManageProposalImpl.class.getName());
     // ~services
     @Inject
     private IProposal proposalsQueries;
     @Inject
     private IRepository<Proposal> proposalRepo;
 
-    
+    @Override
+    public Proposal getById(Key proposalId) {
+        return proposalRepo.get(proposalId);
+    }
+
 	@Override
 	public void addProposal(Proposal inputProposal, Citizen author) {
-    	inputProposal.setAuthor(author);
+        inputProposal.setAuthor(author);
 		inputProposal.setDate(new Date());
+        LOGGER.info("add proposal " + inputProposal);
        	proposalRepo.persist(inputProposal);
 		
 	}
